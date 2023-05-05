8d697114195e36f5ccb13d38b098a42aa8a7685b
eddieg00 <119461197+eddieg00@users.noreply.github.com>
2023-05-05 15:18:22 -0400

    set up user model and server

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+|..+/, "Please enter a valid email."]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
