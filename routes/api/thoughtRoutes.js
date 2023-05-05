f66c96f96f45f950f84f88b9eb24e1273c5645c7
eddieg00 <119461197+eddieg00@users.noreply.github.com>
2023-05-05 15:24:13 -0400

    linked up thought routes to thought controller

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:id').get(getThoughtById).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
